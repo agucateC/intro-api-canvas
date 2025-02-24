b98b8bd60de98c8f52b0b52990b219fec5659138
Carlos <carlosomarcruz07@gmail.com>
2025-02-24 10:45:28 -0600

    Agregando Boostrap 5.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+// Obtener una referencia al elemento <canvas> del documento HTML
+let canvas = document.getElementById("canvas"); 
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+// Obtener el contexto de renderizado 2D del canvas
+let ctx = canvas.getContext("2d"); 
+
+// Establecer el color de relleno en rojo
+ctx.fillStyle = "rgb(40, 106, 128)"; 
+
+// Dibujar un rectángulo relleno en la posición (50, 50) con un ancho y alto de 100 píxeles
+ctx.fillRect(20, 20, 100, 100);
